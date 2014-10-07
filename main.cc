679f95f86aacc0e543efb95c15455338192a686e
heck <martin.heck@kit.edu>
2014-10-07 17:29:02 +0200

    Initial Commit with just the SConstruct, that builds the program and the main.cc, that just prints out some text.

---
+++ main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "This program makes a toy simulation of a drift chamber and "
+		 <<	"then tries some simple reconstruction algorithms." << endl;
+
+	return 0;
+}
