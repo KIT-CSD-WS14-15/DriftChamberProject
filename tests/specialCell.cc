e72a29009d9bfcfe54a69cce6343391c8b0f44f8
drmartinheck <DrMartinHeck@gmail.com>
2014-10-16 01:04:54 +0000

    Added some cout Output to actuall make sure, that the chosen string really displays the expected colour.

--- tests/specialCell.cc
+++ tests/specialCell.cc
@@ -4,16 +4,20 @@
 using namespace std;
 //We want the special cell to be able to handle different colours.
 //This is the first difference to the Cell.
-TEST(SpecialCellTest, visualization)
+TEST(SpecialCellTest, specialVisualization)
 {
   shared_ptr<ColourStrategy> colourStrategyBluePtr (new ColourStrategyBlue());
   shared_ptr<ColourStrategy> colourStrategyRedPtr  (new ColourStrategyRed() );
+  
   SpecialCell specialCell (0,0, colourStrategyBluePtr);
   
   EXPECT_EQ("\x1B[97;44;1mO\x1B[0m", specialCell.visualize());
+  cout << "Blue Cell: " << specialCell.visualize() << endl;
+  
   specialCell.addEDeposition(1);
   EXPECT_EQ("\x1B[97;44;1mX\x1B[0m", specialCell.visualize());
   
   SpecialCell specialCell1 (0,0, colourStrategyRedPtr);
   EXPECT_EQ("\x1B[97;41;1mO\x1B[0m", specialCell1.visualize());
+  cout << "Red Cell:  " << specialCell1.visualize() << endl;
 }
